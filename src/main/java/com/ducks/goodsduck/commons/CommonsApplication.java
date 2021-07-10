4e3878d943914b7b8c9ad06d5797ade18943cc73
W0nee <kwpark96@gmail.com>
2021-07-11 07:12:51 +0900

    add yml config

--- src/main/java/com/ducks/goodsduck/commons/CommonsApplication.java
+++ src/main/java/com/ducks/goodsduck/commons/CommonsApplication.java
@@ -2,13 +2,21 @@ package com.ducks.goodsduck.commons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CommonsApplication {
 
+	private static final String PROPERTIES =
+			"spring.config.location=" +
+					"classpath:/application.yml," +
+					"classpath:/application-db.yml," +
+					"classpath:/application-oauth2.yml";
+
 	public static void main(String[] args) {
 
-		SpringApplication.run(CommonsApplication.class, args);
-
+		new SpringApplicationBuilder(CommonsApplication.class)
+				.properties(PROPERTIES)
+				.run(args);
 	}
 }
