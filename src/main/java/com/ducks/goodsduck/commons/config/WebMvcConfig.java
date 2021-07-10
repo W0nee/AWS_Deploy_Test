bf27722c8a4474f71c6a600f043ab4468b23398e
W0nee <kwpark96@gmail.com>
2021-07-11 07:01:06 +0900

    Test

---
+++ src/main/java/com/ducks/goodsduck/commons/config/WebMvcConfig.java
@@ -0,0 +1,33 @@
+package com.ducks.goodsduck.commons.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean getFilterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean(new AccessControlAllowFilter());
+        registrationBean.setOrder(Integer.MIN_VALUE);
+        //registrationBean.addUrlPatterns("/*");
+        registrationBean.setUrlPatterns(Arrays.asList("/api/v1/*"));
+        return registrationBean;
+    }
+}
+
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("/**");
+//    }
+//}
+
